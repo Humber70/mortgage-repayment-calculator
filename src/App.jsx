0bdcee118c9cd00e1d7f83f2a857c6d5357307ac
Humberto <humber.arraya@gmail.com>
2025-05-08 17:24:26 -0300

    feat: update Mortgage Calculator layout for improved responsiveness and result display

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ function MortgageCalculator() {
 
   return (
     <>
-      <section className="max-w-[375px] m-auto flex flex-col gap-5">
+      <section className="h-screen flex flex-col justify-between">
         <header>
           <h1 className="text-[var(--slate-900)] font-[var(--font-w-700)]">Mortgage Calculator</h1>
           <button onClick={handleClickClearInptus}>Clear All</button>
@@ -197,31 +197,26 @@ function MortgageCalculator() {
             </div>
             <button>Calculate Repayments</button>
           </form>
-
-          <section className="bg-[--slate-900] w-[100%] p-5">
-            <div>
-              <h1 className="text-[--slate-100] text-[1.2rem]">Your results</h1>
-              <p className="text-[--slate-300] pb-6 pt-3">Your results are shown below based on the information you provided. 
-              To adjust the results, edit the form and click “calculate repayments” again.</p>
-            </div>
-
-            <div className="bg-[--slate-1000] border-t rounded border-[--primary-color-lime] border-t-4 p-2">
-              <div>
-                <h1 className="text-[--slate-300]">Your monthly repayments</h1>
-                <span className="text-[--primary-color-lime] text-3xl">{data.resultMonthy}</span>
-              </div>
-              <hr className="my-5 border-t border-[--slate-700] border-[1px]"/>
-              <div>
-                <h1 className="text-[--slate-300]">Total you monthly reapyments</h1>
-                <span className="text-[1.4rem] text-[--white]">{data.resultTotal}</span>
-              </div>
-            </div>
-          </section>
         </main>
+        <section className="bg-[--slate-900] w-[100%] p-5 self-besaline">
+          <div>
+            <h1 className="text-[--slate-100] text-[1.2rem]">Your results</h1>
+            <p className="text-[--slate-300] pb-6 pt-3">Your results are shown below based on the information you provided. 
+            To adjust the results, edit the form and click “calculate repayments” again.</p>
+          </div>
 
-        <footer>
-        Challenge by Frontend Mentor. Coded by Your Name Here.
-        </footer>
+          <div className="bg-[--slate-1000] border-t rounded border-[--primary-color-lime] border-t-4 p-2">
+            <div>
+              <h1 className="text-[--slate-300]">Your monthly repayments</h1>
+              <span className="text-[--primary-color-lime] text-3xl">{data.resultMonthy}</span>
+            </div>
+            <hr className="my-5 border-t border-[--slate-700] border-[1px]"/>
+            <div>
+              <h1 className="text-[--slate-300]">Total you monthly reapyments</h1>
+              <span className="text-[1.4rem] text-[--white]">{data.resultTotal}</span>
+            </div>
+          </div>
+        </section>
       </section>
     </>
   );

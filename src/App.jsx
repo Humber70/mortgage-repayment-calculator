393bafbe0e0fade5fc0effc40766ea034f174984
Humberto <humber.arraya@gmail.com>
2025-05-09 18:43:38 -0300

    feat: integrate ResultsCalculate component for improved results display in Mortgage Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useRef, useState } from "react";
 import { validate } from "./utils/validations";
 import { calculateMortgage } from "./utils/calculateMortgage";
 
+// components
+import ResultsCalculate from "./components/ResultsCalcualte";
+
 const intialData = {
   amount: 0,
   years: 0,
@@ -198,25 +201,7 @@ function MortgageCalculator() {
             <button>Calculate Repayments</button>
           </form>
         </main>
-        <section className="bg-[--slate-900] w-[100%] p-5 self-besaline">
-          <div>
-            <h1 className="text-[--slate-100] text-[1.2rem]">Your results</h1>
-            <p className="text-[--slate-300] pb-6 pt-3">Your results are shown below based on the information you provided. 
-            To adjust the results, edit the form and click “calculate repayments” again.</p>
-          </div>
-
-          <div className="bg-[--slate-1000] border-t rounded border-[--primary-color-lime] border-t-4 p-2">
-            <div>
-              <h1 className="text-[--slate-300]">Your monthly repayments</h1>
-              <span className="text-[--primary-color-lime] text-3xl">{data.resultMonthy}</span>
-            </div>
-            <hr className="my-5 border-t border-[--slate-700] border-[1px]"/>
-            <div>
-              <h1 className="text-[--slate-300]">Total you monthly reapyments</h1>
-              <span className="text-[1.4rem] text-[--white]">{data.resultTotal}</span>
-            </div>
-          </div>
-        </section>
+        <ResultsCalculate results={data}/>
       </section>
     </>
   );

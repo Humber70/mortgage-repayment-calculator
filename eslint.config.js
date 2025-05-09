5d771d2dc5a313b7788632a7bca27c91e26d8292
Humberto <humber.arraya@gmail.com>
2025-05-09 18:43:18 -0300

    fix: enable prop-types rule in ESLint configuration for better type checking

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,8 @@ export default [
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      'react/react-in-jsx-scope': 'off'
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off' 
     }
   }
 ];

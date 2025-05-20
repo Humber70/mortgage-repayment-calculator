3c5af296d89502cfba69d5bfb5a5176b6e0dd25f
Humberto <humber.arraya@gmail.com>
2025-05-20 13:22:41 -0300

    style: update error message handling and improve footer display logic in MortgageCalculator

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,6 @@ function MortgageCalculator() {
       years: Number(field.get("years")),
       rate: Number(field.get("percent")),
     };
-    console.log(inputsFields)
 
     // validation
     const validationErrors = validate(inputsFields);
@@ -91,7 +90,7 @@ function MortgageCalculator() {
     }
 
     // Clean message error of the fields
-    setMessageError({});
+    setMessageError(false);
 
     // calculates if you select the "repayment" option
     if (categorie === "repayment") {
@@ -134,7 +133,19 @@ function MortgageCalculator() {
         </main>
         
         <footer>
+          {messageError ? (
+          <section className="flex flex-col justify-center items-center bg-[--slate-900] p-[1.5rem]">
+            <div>
+              <img src="/assets/images/illustration-empty.svg" alt="image-empty" />
+            </div>
+            <div>
+              <h2 className="font-[--font-w-700] text-2xl text-[white] text-center">Results shown here</h2>
+              <p className="py-3 text-center text-[var(--slate-500)]">{'Complete the form and click "calculate repayments" to see what your repayments would be.'}</p>
+            </div>
+          </section>
+          ) : (
           <ResultsCalculate results={data}/>
+          )}
         </footer>
       </section>
     </>

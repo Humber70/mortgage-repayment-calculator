61eba804f35772c1eb91457c0e23b236bb50c3be
Humberto <humber.arraya@gmail.com>
2025-05-20 23:14:09 -0300

    style: replace inline empty state illustration with IllustrationEmpty component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { calculateMortgage } from "./utils/calculateMortgage";
 // components
 import ResultsCalculate from "./components/ResultsCalcualte";
 import FormCalculate from "./components/FormCalculate";
+import IllustrationEmpty from "./components/IllustrationEmpty";
 
 const intialData = {
   amount: 0,
@@ -133,19 +134,7 @@ function MortgageCalculator() {
         </main>
         
         <footer>
-          {messageError ? (
-          <section className="flex flex-col justify-center items-center bg-[--slate-900] p-[1.5rem]">
-            <div>
-              <img src="/assets/images/illustration-empty.svg" alt="image-empty" />
-            </div>
-            <div>
-              <h2 className="font-[--font-w-700] text-2xl text-[white] text-center">Results shown here</h2>
-              <p className="py-3 text-center text-[var(--slate-500)]">{'Complete the form and click "calculate repayments" to see what your repayments would be.'}</p>
-            </div>
-          </section>
-          ) : (
-          <ResultsCalculate results={data}/>
-          )}
+          {messageError ? <IllustrationEmpty /> : <ResultsCalculate results={data}/>}
         </footer>
       </section>
     </>
